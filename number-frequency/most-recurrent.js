e88a079d95265dfdf7bbdc7240452b28c8c487d1
Jirffey Gabuya <boyhimas@gmail.com>
2018-06-05 23:19:42 +0800

    number-frequency solution

--- number-frequency/most-recurrent.js
+++ number-frequency/most-recurrent.js
@@ -1,5 +1,11 @@
 const mostRecurrent = (set) => {
-  return set
+    const occ = Array(Math.max(...set) + 1).fill(0);
+
+    set.map((item) => {
+        occ[item]++;
+    });
+
+    return occ.indexOf(Math.max(...occ));
 }
 
 module.exports = mostRecurrent
