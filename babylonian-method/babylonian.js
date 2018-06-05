c2007418ed90896f6ad65caf14881946b4edc161
Jirffey Gabuya <boyhimas@gmail.com>
2018-06-06 00:58:54 +0800

    babylonian-method solution

--- babylonian-method/babylonian.js
+++ babylonian-method/babylonian.js
@@ -1,5 +1,18 @@
 const squareRoot = (radicand) => {
-  return radicand
+    const calc = (radicand, sqrt) => {
+        if (radicand < 0 || isNaN(radicand)) return NaN;
+
+        if (! sqrt) sqrt = radicand / 2.0;
+
+        const res = radicand / sqrt;
+        const avg = (res + sqrt) / 2.0;
+
+        if (sqrt === avg) return sqrt;
+
+        return calc(radicand, avg);
+    };
+
+    return calc(radicand);
 }
 
 module.exports = squareRoot
