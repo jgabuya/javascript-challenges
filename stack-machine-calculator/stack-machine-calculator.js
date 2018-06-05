35e5e5d3b19ea054ab9aa788250fb6efd11b9e9e
Jirffey Gabuya <boyhimas@gmail.com>
2018-06-06 01:51:00 +0800

    Refactor
    No need to return the array as Javascript passes arrays by reference

--- stack-machine-calculator/stack-machine-calculator.js
+++ stack-machine-calculator/stack-machine-calculator.js
@@ -3,48 +3,45 @@ const stackMachineCalculator = (instructions) => {
     let resStack = [];
 
     const doAdd = stack => {
-        return stack.push(stack.pop() + stack.pop());
+        stack.push(stack.pop() + stack.pop());
     };
 
     const doSub = stack => {
-        return stack.push(stack.pop() - stack.pop());
+        stack.push(stack.pop() - stack.pop());
     };
 
     const doPop = stack => {
         stack.pop();
-        return stack();
     };
 
     const doDup = stack => {
         stack.push(stack[stack.length - 1]);
-        return stack;
     };
 
     const doInt = (stack, val) => {
         stack.push(val);
-        return stack;
     };
 
     const doProcess = (stack, val) => {
         switch (val) {
             case 'ADD':
-                stack = doAdd(stack);
+                doAdd(stack);
                 break;
 
             case 'SUB':
-                stack = doSub(stack);
+                doSub(stack);
                 break;
 
             case 'POP':
-                stack = doPop(stack);
+                doPop(stack);
                 break;
 
             case 'DUP':
-                stack = doDup(stack);
+                doDup(stack);
                 break;
 
             default:
-                stack = doInt(stack, Number(val));
+                doInt(stack, Number(val));
                 break;
         }
 
@@ -52,10 +49,10 @@ const stackMachineCalculator = (instructions) => {
     };
 
     instStack.forEach((instruction) => {
-        resStack = doProcess(resStack, instruction);
+        doProcess(resStack, instruction);
     });
 
-    return resStack[resStack.length - 1];
+    return resStack.pop();
 }
 
 module.exports = stackMachineCalculator
