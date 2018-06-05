daa478c7b0f1f81984ac6aaf531ff252aec2b253
Jirffey Gabuya <boyhimas@gmail.com>
2018-06-06 01:44:11 +0800

    stack-machine-calculator solution

--- stack-machine-calculator/stack-machine-calculator.js
+++ stack-machine-calculator/stack-machine-calculator.js
@@ -1,5 +1,61 @@
 const stackMachineCalculator = (instructions) => {
-  return instructions
+    const instStack = instructions.trim().split(' ');
+    let resStack = [];
+
+    const doAdd = stack => {
+        return stack.push(stack.pop() + stack.pop());
+    };
+
+    const doSub = stack => {
+        return stack.push(stack.pop() - stack.pop());
+    };
+
+    const doPop = stack => {
+        stack.pop();
+        return stack();
+    };
+
+    const doDup = stack => {
+        stack.push(stack[stack.length - 1]);
+        return stack;
+    };
+
+    const doInt = (stack, val) => {
+        stack.push(val);
+        return stack;
+    };
+
+    const doProcess = (stack, val) => {
+        switch (val) {
+            case 'ADD':
+                stack = doAdd(stack);
+                break;
+
+            case 'SUB':
+                stack = doSub(stack);
+                break;
+
+            case 'POP':
+                stack = doPop(stack);
+                break;
+
+            case 'DUP':
+                stack = doDup(stack);
+                break;
+
+            default:
+                stack = doInt(stack, Number(val));
+                break;
+        }
+
+        return stack;
+    };
+
+    instStack.forEach((instruction) => {
+        resStack = doProcess(resStack, instruction);
+    });
+
+    return resStack[resStack.length - 1];
 }
 
 module.exports = stackMachineCalculator
